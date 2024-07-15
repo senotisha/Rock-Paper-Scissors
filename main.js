214b18ffc3b95fecfb8e8f5b955bf28b9777eeaa
senotisha <173420359+senotisha@users.noreply.github.com>
2024-07-15 04:45:47 +0200

    Add HumanChoice

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 function getComputerChoice () {
     const computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice == 1) {
+    if (computerChoice === 1) {
         return "ROCK"
     }
 
-    else if (computerChoice == 2) {
+    else if (computerChoice === 2) {
         return "PAPER"
     }
 
-    else if (computerChoice == 3) 
+    else (computerChoice === 3) 
         return "SCISSORS"
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice (){
+    let humanChoice = prompt ("Pick: Rock, Paper OR Scissors");
+    if (humanChoice === "Rock") {
+        return "ROCK"
+    }
+    
+    else if (humanChoice == "Paper") {
+        return "PAPER"
+    }
+
+    else (humanChoice === "Scissors") 
+        return "SCISSORS"
+    
+}
+
+console.log(getHumanChoice())
