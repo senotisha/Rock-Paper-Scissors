57d53c7f9b42114cb32d269053a992555c49a8b5
senotisha <173420359+senotisha@users.noreply.github.com>
2024-07-21 02:17:18 +0200

    Edit playGame function

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
 
 
 function getComputerChoice () {
     const botChoice = Math.floor(Math.random() * 3);
-    if (botChoice === 1) {
+    if (botChoice === 0) {
         return "ROCK";
     }
 
-    else if (botChoice === 2) {
+    else if (botChoice === 1) {
         return "PAPER";
     }
 
@@ -25,8 +27,9 @@ function getHumanChoice (){
         return "PAPER";
     }
 
-    else if (playerChoice === "scissors") 
+    else if (playerChoice === "scissors") {
         return "SCISSORS";
+    }
 
     else {
         console.log("Invalid option")
@@ -34,26 +37,32 @@ function getHumanChoice (){
 }
 
 
-
+function playGame() {
 
 function playRound (humanChoice, computerChoice) {
     if (humanChoice === "ROCK" && computerChoice === "PAPER" ) {
         console.log("You lose! Paper beats Rock.");
+        computerScore++;
     }
     else if (humanChoice === "PAPER" && computerChoice === "SCISSORS" ) {
         console.log("You lose! Scissors beats Paper.");
+        computerScore++;
     }
     else if (humanChoice === "SCISSORS" && computerChoice === "ROCK" ) {
         console.log("You lose! Rock beats Scissors.");
+        computerScore++;
     }
     else if (humanChoice === "PAPER" && computerChoice === "ROCK" ) {
         console.log("You win! Paper beats Rock.");
+        humanScore++;
     }
     else if (humanChoice === "SCISSORS" && computerChoice === "PAPER" ) {
         console.log("You win! Scissors beats Paper.");
+        humanScore++;
     }
     else if (humanChoice === "ROCK" && computerChoice === "SCISSORS" ) {
         console.log("You win! Rock beats Scissors.");
+        humanScore++;
     }
 
     else if (humanChoice === computerChoice) {
@@ -65,18 +74,17 @@ function playRound (humanChoice, computerChoice) {
     }
 }
 
+for (let i = 0; i < 5; i++) {
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
-
 playRound (humanSelection, computerSelection);
-
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    playRound (getComputerChoice(), getHumanChoice());
-    playRound (getComputerChoice(), getHumanChoice());
-    playRound (getComputerChoice(), getHumanChoice());
-    playRound (getComputerChoice(), getHumanChoice());
-
+    }
+    if (humanScore > computerScore) {
+        console.log("Player Wins!")
+    }
+    else {
+        console.log("Computer Wins!")
+    }
 }
+
+playGame();
